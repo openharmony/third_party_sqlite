4c8729e9a5497a5807f20f2f0166fdcaa90e6cf3
wddhw <wangxiangdong13@huawei.com>
2026-05-07 11:09:24 +0800

    add binlog search ability
    Signed-off-by: wddhw <wangxiangdong13@huawei.com>

--- include/sqlite3sym.h
+++ include/sqlite3sym.h
@@ -35,6 +35,7 @@ typedef enum BinlogFileCleanMode {
 
 typedef enum {
   ROW = 0,
+  ROW_FOR_SEARCH = 2
 } Sqlite3BinlogMode;
 
 typedef struct Sqlite3BinlogConfig {
@@ -45,6 +46,44 @@ typedef struct Sqlite3BinlogConfig {
     void (*xLogFullCallback)(void *pCtx, unsigned short currentCount, const char *dbPath);
     void *callbackCtx;
 } Sqlite3BinlogConfig;
+
+typedef struct BinlogSearchResult {
+  int op;                // one of the SQLITE_INSERT, SQLITE_UPDATE, SQLITE_DELETE
+  sqlite3_int64 rowid;   // rowid of changed data, -1 for without rowid table
+  sqlite3_uint64 nCol;  // size of pData
+  const char *tableName;
+  int fileIndex;
+  sqlite3_uint64 readPos;
+  char **nameAndValues; // pointer to the row data in record format
+} BinlogSearchResult;
+
+typedef struct BinlogSearchResultSet{
+  int capacity;      // capacity of data
+  int row_count;     // count of data
+  BinlogSearchResult *results;       // results array
+} BinlogSearchResultSet;
+
+typedef enum BinlogHwmSetMode {
+  BINLOG_PERSIST_MODE = 0,
+  BINLOG_CACHE_MODE = 1,
+  BINLOG_MODE_MAX,
+} BinlogHwmSetModeE;
+
+typedef struct BinlogSearchHwm {
+  int readFileIndex;
+  sqlite3_uint64 readPos;
+} BinlogSearchHwmT;
+
+typedef struct MonitorTableCol {
+  const char *tableName;    // tableName
+  const char **cols;        // colsArray
+  int colCount;       // colsCount
+} MonitorTableCol;
+
+typedef struct MonitorTablesConfig {
+  MonitorTableCol *tables;  // tableCols
+  int tableCount;           // tableCount
+} MonitorTablesConfig;
 /*
 ** END OF BINLOG CONFIG
 *************************************************************************/
@@ -77,6 +116,13 @@ struct sqlite3_api_routines_extra {
   int (*rekey_v3)(CodecRekeyConfig *);
   int (*is_support_binlog)(const char*);
   int (*replay_binlog)(sqlite3*, sqlite3*);
+  int (*set_monitor_config)(sqlite3*, MonitorTablesConfig*);
+  int (*set_xChange_callback)(sqlite3*, void (*xChangeCallback)(const char *dbPath, char *tableName));
+  int (*set_json_parse_callback)(sqlite3*, MonitorTablesConfig*(*jsonParseCallback)(const char *dbPath));
+  int (*get_search_data)(sqlite3*, sqlite3*, BinlogSearchResultSet**);
+  int (*free_search_data)(sqlite3*, BinlogSearchResultSet**);
+  int (*set_search_hwm)(sqlite3*, BinlogSearchHwmT*, BinlogHwmSetModeE);
+  int (*reset_search_hwm)(sqlite3*);
   int (*clean_binlog)(sqlite3*, BinlogFileCleanModeE);
   int (*compressdb_backup)(sqlite3*, const char*);
 };
@@ -91,6 +137,13 @@ extern const struct sqlite3_api_routines_extra *sqlite3_export_extra_symbols;
 #define sqlite3_rekey_v3            sqlite3_export_extra_symbols->rekey_v3
 #define sqlite3_is_support_binlog   sqlite3_export_extra_symbols->is_support_binlog
 #define sqlite3_replay_binlog       sqlite3_export_extra_symbols->replay_binlog
+#define sqlite3_set_monitor_config_binlog  sqlite3_export_extra_symbols->set_monitor_config
+#define sqlite3_set_xChange_callback_binlog  sqlite3_export_extra_symbols->set_xChange_callback
+#define sqlite3_set_json_parse_callback_binlog  sqlite3_export_extra_symbols->set_json_parse_callback
+#define sqlite3_get_search_data_binlog  sqlite3_export_extra_symbols->get_search_data
+#define sqlite3_free_search_data_binlog sqlite3_export_extra_symbols->free_search_data
+#define sqlite3_set_search_hwm_binlog   sqlite3_export_extra_symbols->set_search_hwm
+#define sqlite3_reset_search_hwm_binlog sqlite3_export_extra_symbols->reset_search_hwm
 #define sqlite3_clean_binlog        sqlite3_export_extra_symbols->clean_binlog
 #define sqlite3_compressdb_backup   sqlite3_export_extra_symbols->compressdb_backup
 
